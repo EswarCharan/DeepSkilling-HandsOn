ed8846a6631a19f6f1fddbc00aa6cdcdc88bb97f
EswarCharan <162131374+EswarCharan@users.noreply.github.com>
2025-07-26 11:24:31 +0530

    Create App.js

---
+++ Week 6/React 3/App.js
@@ -0,0 +1,14 @@
+import { CalculateScore } from "./Components/CalculatorScore";
+function App() {
+  return (
+    <div>
+      <CalculateScore Name={"Steeve"}
+      School={"Gloria English Medium Schoool"}
+      total={284}
+      goal={3}
+      />
+    </div>
+  );
+}
+
+export default App;
